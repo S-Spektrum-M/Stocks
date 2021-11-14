1cc8de71048bf9406dada98e30ff4869723f05ac
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-14 15:16:50 -0800

    Add a file containing all stats functions

---
+++ stats.py
@@ -0,0 +1,59 @@
+import numpy as np
+import statistics as stat
+from robin_stocks import robinhood as rh
+import auth
+
+rh.authentication.login(auth.USERNAME, auth.PASSWORD)
+
+def beta(ticker):
+    """
+    param: string: ticker: string that represents ticker to query
+    return: float: beta of stock relative to sector
+    """
+    return 1
+
+def short_linear_reg(ticker):
+    """
+    param: ticker: string that represents ticker to query
+    return: short term linear regression prediction
+    """
+    prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
+
+    y_tmp = list()
+    i = 0
+    for price in prices:
+        y_tmp.append(float(price["open_price"]))
+        y_tmp.append(float(price["close_price"]))
+        i = i + 2
+
+    x = np.arange(0, i)
+    y = np.asarray(y_tmp)
+
+    r = np.corrcoef(x, y)[0][1]
+    s_x = np.std(x)
+    s_y = np.std(y)
+    slope = r * s_y / s_x
+    return [slope * (i+1 - np.mean(x)) + np.mean(y) + (2 * s_y * beta(ticker)), slope * (i+1 - np.mean(x)) + np.mean(y) - (2 * s_y * beta(ticker))]
+
+def long_linear_reg(ticker):
+    """
+    param: ticker: string that represents ticker to query
+    return: long term linear regression prediction
+    """
+    prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
+
+    y_tmp = list()
+    i = 0
+    for price in prices:
+        y_tmp.append(float(price["open_price"]))
+        y_tmp.append(float(price["close_price"]))
+        i = i + 2
+
+    x = np.arange(0, i)
+    y = np.asarray(y_tmp)
+
+    r = np.corrcoef(x, y)[0][1]
+    s_x = np.std(x)
+    s_y = np.std(y)
+    slope = r * s_y / s_x
+    return [slope * (i+1 - np.mean(x)) + np.mean(y) + (2 * s_y * beta(ticker)), slope * (i+1 - np.mean(x)) + np.mean(y) - (2 * s_y * beta(ticker))]
