9540541e6fb8a17c571170d717b77f4f92cab138
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-13 17:03:34 -0800

    Modify main.py to print historical

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from robin_stocks import robinhood as rh
 import auth
 
 rh.authentication.login(auth.USERNAME, auth.PASSWORD)
+f = open('stocks.json')
+stocks = json.load(f)
+
+for stock in stocks:
+    print(rh.stocks.get_latest_price(stock["Symbol"])[0])
+    rh.stocks.get_stock_historicals
