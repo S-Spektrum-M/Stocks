fcb906eed5370a84831d65e79286fddccb0f8220
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-16 19:48:35 -0800

    Update short_linear_reg and long_linear_reg   - Add redis checking

--- stats.py
+++ stats.py
@@ -2,15 +2,23 @@ import numpy as np
 import statistics as stat
 from robin_stocks import robinhood as rh
 import auth
+import redis
+from datetime import datetime
 
 rh.authentication.login(auth.USERNAME, auth.PASSWORD)
+client = redis.Redis(host='localhost', port=6379, db=0)
+now = datetime.now()
+
 
 def short_linear_reg(ticker):
     """
     param: ticker: string that represents ticker to query
     return: short term linear regression prediction
     """
-    prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
+    try:
+        prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
+    except TypeError:
+        return [0,0].append("bad_request")
 
     y_tmp = list()
     i = 0
@@ -21,19 +29,32 @@ def short_linear_reg(ticker):
 
     x = np.arange(0, i)
     y = np.asarray(y_tmp)
-
     r = np.corrcoef(x, y)[0][1]
     s_x = np.std(x)
     s_y = np.std(y)
+
     slope = r * s_y / s_x
-    return [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
+
+    query = (f'short-{ticker}-{datetime.now().strftime("%H:%M")}')
+    if (client.get(query)) == None:
+        b = [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
+        client.set(query,str(b))
+        print(f'Added {ticker} to db')
+        return b
+    else:
+        print(f'loaded {ticker} from db')
+        print(query)
+        return client.get(query)
 
 def long_linear_reg(ticker):
     """
     param: ticker: string that represents ticker to query
     return: long term linear regression prediction
     """
-    prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
+    try:
+        prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
+    except TypeError:
+        return [0,0].append("bad_request")
 
     y_tmp = list()
     i = 0
@@ -44,9 +65,18 @@ def long_linear_reg(ticker):
 
     x = np.arange(0, i)
     y = np.asarray(y_tmp)
-
     r = np.corrcoef(x, y)[0][1]
     s_x = np.std(x)
     s_y = np.std(y)
     slope = r * s_y / s_x
-    return [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
+
+    query = (f'long-{ticker}-{datetime.now().strftime("%H:%M")}')
+    if (client.get(query)) == None:
+        b = [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
+        client.set(query,str(b))
+        print(f'Added {ticker} to db')
+        return b
+    else:
+        print(f'loaded {ticker} from db')
+        print(query)
+        return client.get(query)
