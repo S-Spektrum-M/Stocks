b5f64d27627c3cfe0b82fdc5c2bd7c60f92df517
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-16 21:01:15 -0800

    Performance Add stats code in if block to avoid running if in DB

--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ from robin_stocks import robinhood as rh
 import auth
 import redis
 from datetime import datetime
+import math
 
 rh.authentication.login(auth.USERNAME, auth.PASSWORD)
 client = redis.Redis(host='localhost', port=6379, db=0)
@@ -15,28 +16,28 @@ def short_linear_reg(ticker):
     param: ticker: string that represents ticker to query
     return: short term linear regression prediction
     """
-    try:
-        prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
-    except TypeError:
-        return None
-
-    y_tmp = list()
-    i = 0
-    for price in prices:
-        y_tmp.append(float(price["open_price"]))
-        y_tmp.append(float(price["close_price"]))
-        i = i + 2
-
-    x = np.arange(0, i)
-    y = np.asarray(y_tmp)
-    r = np.corrcoef(x, y)[0][1]
-    s_x = np.std(x)
-    s_y = np.std(y)
-
-    slope = r * s_y / s_x
-
     query = (f'short-{ticker}-{datetime.now().strftime("%H:%M")}')
     if (client.get(query)) == None:
+        try:
+            prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
+        except TypeError:
+            return None
+
+        y_tmp = list()
+        i = 0
+        for price in prices:
+            y_tmp.append(float(price["open_price"]))
+            y_tmp.append(float(price["close_price"]))
+            i = i + 2
+
+        x = np.arange(0, i)
+        y = np.asarray(y_tmp)
+        r = np.corrcoef(x, y)[0][1]
+        s_x = np.std(x)
+        s_y = np.std(y)
+
+        slope = r * s_y / s_x
+
         b = [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
         client.set(query,str(b))
         print(f'Added {ticker} to db')
@@ -44,34 +45,35 @@ def short_linear_reg(ticker):
     else:
         print(f'loaded {ticker} from db')
         print(query)
-        return client.get(query)
+        return list(client.get(query))
+
 
 def long_linear_reg(ticker):
     """
     param: ticker: string that represents ticker to query
-    return: long term linear regression prediction
+    return: short term linear regression prediction
     """
-    try:
-        prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
-    except TypeError:
-        return None
-
-    y_tmp = list()
-    i = 0
-    for price in prices:
-        y_tmp.append(float(price["open_price"]))
-        y_tmp.append(float(price["close_price"]))
-        i = i + 2
-
-    x = np.arange(0, i)
-    y = np.asarray(y_tmp)
-    r = np.corrcoef(x, y)[0][1]
-    s_x = np.std(x)
-    s_y = np.std(y)
-    slope = r * s_y / s_x
-
     query = (f'long-{ticker}-{datetime.now().strftime("%H:%M")}')
     if (client.get(query)) == None:
+        try:
+            prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
+        except TypeError:
+            return None
+
+        y_tmp = list()
+        i = 0
+        for price in prices:
+            y_tmp.append(float(price["open_price"]))
+            y_tmp.append(float(price["close_price"]))
+            i = i + 2
+
+        x = np.arange(0, i)
+        y = np.asarray(y_tmp)
+        r = np.corrcoef(x, y)[0][1]
+        s_x = np.std(x)
+        s_y = np.std(y)
+        slope = r * s_y / s_x
+
         b = [slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y)]
         client.set(query,str(b))
         print(f'Added {ticker} to db')
@@ -79,4 +81,6 @@ def long_linear_reg(ticker):
     else:
         print(f'loaded {ticker} from db')
         print(query)
-        return client.get(query)
+        return list(client.get(query))
+
+print(long_linear_reg('aapl'))
