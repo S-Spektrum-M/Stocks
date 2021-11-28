e7d87094a175d11350c40cc036b17b46ee954756
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-28 00:46:36 -0800

    njit

--- stats.py
+++ stats.py
@@ -2,14 +2,42 @@ import numpy as np
 from robin_stocks import robinhood as rh
 import auth
 import redis
-from datetime import datetime, date
+from datetime import datetime
+from numba import njit
 
 rh.authentication.login(auth.USERNAME, auth.PASSWORD)
 client = redis.Redis(host='localhost', port=6379, db=0)
 now = datetime.now()
 
+@njit()
+def calc_short(y, i):
+    x = np.arange(0, i)
+    r = np.corrcoef(x, y)[0][1]
+    s_x = np.std(x)
+    s_y = np.std(y)
+    slope = r * s_y / s_x
+    b = [
+        round(slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), 2),
+        round(slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y), 2)
+    ]
+    return b
+
+@njit()
+def calc_long(y, i):
+    rate_of_change = y[i] ** (1/float(i))
+    b = [
+        round(y[i] * rate_of_change + np.std(y),2),
+        round(y[i] * rate_of_change - np.std(y),2),
+    ]
+    return b
+
+def update_db(query, vals):
+    client.set(query, "true")
+    client.set(f'{query}.upper',vals[0])
+    client.set(f'{query}.lower',vals[1])
+
 def short(ticker):
-    query = (f'long-{ticker}-{datetime.now().strftime("%d/%m/%Y:%H")}')
+    query = (f'short-{ticker}-{datetime.now().strftime("%d/%m/%Y:%H")}')
     if client.get(query) != None:
         print(query)
         return [
@@ -28,20 +56,8 @@ def short(ticker):
                 y_tmp.append(float(price["close_price"]))
                 i = i + 2
 
-            x = np.arange(0, i)
-            y = np.asarray(y_tmp)
-            r = np.corrcoef(x, y)[0][1]
-            s_x = np.std(x)
-            s_y = np.std(y)
-            slope = r * s_y / s_x
-            b = [
-                round(slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), 2),
-                round(slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y), 2)
-            ]
-
-            client.set(query, "true")
-            client.set(f'{query}.upper',b[0])
-            client.set(f'{query}.lower',b[1])
+            b = calc_short(np.asarray(y_tmp), i)
+            update_db(query, b)
             return b
 
 def long(ticker):
@@ -63,13 +79,6 @@ def long(ticker):
                 y_tmp.append(float(price["open_price"]))
                 y_tmp.append(float(price["close_price"]))
                 i = i + 2
-            rate_of_change = y_tmp[i] ** (1/float(i))
-            y = np.asarray(y_tmp)
-            b = [
-                round(y_tmp[i] * rate_of_change + np.std(y),2),
-                round(y_tmp[i] * rate_of_change - np.std(y),2),
-            ]
-            client.set(query, "true")
-            client.set(f'{query}.upper',b[0])
-            client.set(f'{query}.lower',b[1])
+            b = calc_long(np.asarray(y_tmp), i)
+            update_db(query, b)
             return b
