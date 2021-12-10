8cf3ddb47393ab916e7b7ac4cc601695e85f4d23
Siddharth Mohanty <neosiddharth@gmail.com>
2021-12-09 18:19:10 -0800

    Updates

--- stats.py
+++ stats.py
@@ -1,56 +1,63 @@
+"""
+Statistical Calculations for stock predictions
+methods:
+update_db
+"""
 import numpy as np
 from robin_stocks import robinhood as rh
 import auth
 import redis
 from datetime import datetime
 from numba import njit
+from sklearn.linear_model import LinearRegression as lr
 
 rh.authentication.login(auth.USERNAME, auth.PASSWORD)
-client = redis.Redis(host='localhost', port=6379, db=0)
-now = datetime.now()
+
+CLIENT = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def update_db(query, vals):
-    client.set(query, "true")
-    client.set(f'{query}.upper',vals[0])
-    client.set(f'{query}.lower',vals[1])
+    CLIENT.set(query, "true")
+    CLIENT.set(f'{query}.upper', vals[0])
+    CLIENT.set(f'{query}.lower', vals[1])
+
 
 def update_db_bad_id(ticker):
-    client.set(ticker, "bad_id")
+    CLIENT.set(ticker, "bad_id")
 
 @njit()
 def calc_short(y, i):
-    x = np.arange(0, i)
-    r = np.corrcoef(x, y)[0][1]
-    s_x = np.std(x)
-    s_y = np.std(y)
-    slope = r * s_y / s_x
-    b = [
-        round(slope * (i+1 - np.mean(x)) + np.mean(y) + (s_y), 2),
-        round(slope * (i+1 - np.mean(x)) + np.mean(y) - (s_y), 2)
-    ]
-    return b
+    # calc LinearRegression of y
+    x = np.arange(i)
+    x = x.reshape(-1, 1)
+    reg = lr().fit(x, y)
+    # calculate upper and lower bounds
+    upper = reg.predict(x)
+    lower = reg.predict(x)
+    upper[0] = upper[0] + (upper[0] * 0.05)
+    lower[0] = lower[0] - (lower[0] * 0.05)
+    return [upper[0], lower[0]]
 
 @njit()
 def calc_long(y, i):
     if y[0] != 0:
-        rate_of_change = (y[i]/y[0]) ** (1/float(i))
+        rate_of_change = (y[i] / y[0])**(1 / float(i))
         b = [
-            round(y[i] * rate_of_change + np.std(y),2),
-            round(y[i] * rate_of_change - np.std(y),2),
+            round(y[i] * rate_of_change + np.std(y), 2),
+            round(y[i] * rate_of_change - np.std(y), 2),
         ]
         return b
     else:
         return None
 
 def short(ticker):
-    if client.get(ticker) != 'bad_id':
+    if CLIENT.get(ticker) != 'bad_id':
         query = (f'short-{ticker}-{datetime.now().strftime("%d/%m/%Y:%H")}')
-        if client.get(query) != None:
+        if CLIENT.get(query) != None:
             print(query)
             return [
-                float(client.get(f'{query}.upper').decode('utf-8')),
-                float(client.get(f'{query}.lower').decode('utf-8'))
+                float(CLIENT.get(f'{query}.upper').decode('utf-8')),
+                float(CLIENT.get(f'{query}.lower').decode('utf-8'))
             ]
         else:
             prices = rh.stocks.get_stock_historicals(ticker, 'hour', 'month')
@@ -72,13 +79,13 @@ def short(ticker):
         return None
 
 def long(ticker):
-    if client.get(ticker) != 'bad_id':
+    if CLIENT.get(ticker) != 'bad_id':
         query = (f'long-{ticker}-{datetime.now().strftime("%d/%m/%Y")}')
-        if client.get(query) != None:
+        if CLIENT.get(query) != None:
             print(query)
             return [
-                float(client.get(f'{query}.upper').decode('utf-8')),
-                float(client.get(f'{query}.lower').decode('utf-8'))
+                float(CLIENT.get(f'{query}.upper').decode('utf-8')),
+                float(CLIENT.get(f'{query}.lower').decode('utf-8'))
             ]
         else:
             prices = rh.stocks.get_stock_historicals(ticker, 'day', '5year')
@@ -99,4 +106,5 @@ def long(ticker):
     else:
         return None
 
-update_db_bad_id('aapl')
+def curr(ticker):
+    return rh.get_latest_price(ticker)[0]
