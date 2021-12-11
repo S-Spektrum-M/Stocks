58ba7ac8432eca77ec21216de51d2dd6209ae1fc
Siddharth Mohanty <neosiddharth@gmail.com>
2021-12-10 21:06:12 -0800

    Update: AutoPep8

--- api.py
+++ api.py
@@ -5,11 +5,11 @@ https://github.com/S-Spektrum-M/Stocks
 """
 import stats as st
 import flask
-import json
 from flask import request, jsonify
 
 APP = flask.Flask(__name__)
 # app.config["DEBUG"] = True
+
 
 @APP.route('/api/short/multi', methods=['GET'])
 def short_multi():
@@ -23,27 +23,15 @@ def short_multi():
                 if param.isalpha():
                     response = st.short(param.upper())
                     if response is not None:
-                        ret_list.append({
-                            param: response
-                        })
+                        ret_list.append({param: response})
                     else:
-                        ret_list.append({
-                            "error": "bad_id",
-                            "bad_id": param
-                        })
+                        ret_list.append({"error": "bad_id", "bad_id": param})
                 else:
-                    ret_list.append({
-                        "error": "bad_id",
-                        "bad_id": param
-                    })
+                    ret_list.append({"error": "bad_id", "bad_id": param})
             return jsonify(ret_list), 200
-        return jsonify({
-            "error": "too long",
-            "size": len(params_list)
-        })
-    return jsonify({
-        "error": "no_param"
-    }), 404
+        return jsonify({"error": "too long", "size": len(params_list)})
+    return jsonify({"error": "no_param"}), 404
+
 
 @APP.route('/api/short', methods=['GET'])
 def short():
@@ -51,23 +39,19 @@ def short():
     if 'id' in request.args:
         ticker = str(request.args['id'])
         if ticker.isalpha():
-            response = st.short(ticker.upper()) # Upper case for better DB Caching
+            response = st.short(
+                ticker.upper())  # Upper case for better DB Caching
             if response is not None:
                 return jsonify({
                     'upper': response[0],
                     'lower': response[1]
                 }), 200
-            return jsonify({
-                "error": "bad_id"
-            }), 404
-        return jsonify({
-            "error": "no_id"
-        }), 400
+            return jsonify({"error": "bad_id"}), 404
+        return jsonify({"error": "no_id"}), 400
 
     else:
-        return jsonify({
-            "error": "no_id"
-        }), 400
+        return jsonify({"error": "no_id"}), 400
+
 
 @APP.route('/api/long/multi', methods=['GET'])
 def long_multi():
@@ -80,19 +64,13 @@ def long_multi():
             if param.isalpha():
                 response = st.long(param.upper())
                 if response != None:
-                    ret_list.append({
-                        param: response
-                    })
+                    ret_list.append({param: response})
             else:
-                ret_list.append({
-                    "error": "bad_id",
-                    "bad_id": param
-                }), 404
+                ret_list.append({"error": "bad_id", "bad_id": param}), 404
         return jsonify(ret_list), 200
     else:
-        return jsonify({
-            "error": "no_id"
-        }), 404
+        return jsonify({"error": "no_id"}), 404
+
 
 @APP.route('/api/long', methods=['GET'])
 def long():
@@ -107,17 +85,12 @@ def long():
                     'lower': response[1]
                 }), 200
             else:
-                return jsonify({
-                    "error": "bad_id"
-                }), 404
+                return jsonify({"error": "bad_id"}), 404
         else:
-            return jsonify({
-                "error": "no_id"
-            }), 400
+            return jsonify({"error": "no_id"}), 400
     else:
-        return jsonify({
-            "error": "no_id"
-        }), 400
+        return jsonify({"error": "no_id"}), 400
+
 
 @APP.route('/api/current')
 def current():
@@ -127,17 +100,10 @@ def current():
         if ticker.isalpha():
             respone = st.current(ticker)
             if respone is not None:
-                return jsonify({
-                    'current_price':  respone
-                })
-            return jsonify({
-                "error": "bad_id"
-            }), 404
-        return jsonify({
-            "error": "bad_id"
-        }), 404
-    return jsonify({
-        "error": "no_id"
-    }), 400
+                return jsonify({'current_price': respone})
+            return jsonify({"error": "bad_id"}), 404
+        return jsonify({"error": "bad_id"}), 404
+    return jsonify({"error": "no_id"}), 400
+
 
 APP.run(host="localhost", port=8080, debug=True)
